0e602f42c51fe500172d7f4d9be86de0b66dc372
viniciusteixeiradias <viniciustdias2001@gmail.com>
2024-03-25 15:25:34 +1000

    chore: first commit

---
+++ release.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  verifyConditions: [
+    "semantic-release-expo",
+    "@semantic-release/changelog",
+    "@semantic-release/git",
+    "@semantic-release/npm"
+  ],
+  prepare: [
+    "semantic-release-expo",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    {
+      path: "@semantic-release/git",
+      assets: [
+        "CHANGELOG.md",
+        "package.json",
+        "package-lock.json",
+        "app.json"
+      ]
+    }
+  ],
+  publish: false,
+  success: false,
+  fail: false
+};
